a006a3b95e1fdfa2cffd347c231397b38f1c458e
shogo.tsuji <tjsg06@star.cims.jp>
2021-08-01 10:37:07 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+    @pagy, @users = pagy(User.order(id: :desc), item: 25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました'
+      redirect_tp @user
+    else
+      flash[:danger] = 'ユーザを登録できませんでした'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
