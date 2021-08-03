d1274c62a53142d4ca0463b626d6b23e69940ed1
shogo.tsuji <tjsg06@star.cims.jp>
2021-08-03 12:49:32 +0000

    tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
-  
-  resources :users
 
   resources :tasks
 end
