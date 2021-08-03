0e994ce3ab8575a71cdb3e92a13f4fdf5e63a0ac
shogo.tsuji <tjsg06@star.cims.jp>
2021-08-03 05:11:29 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,13 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+    
     def index
         @tasks = Task.all
+        if logged_in?
+          @task = current_user.tasks.build  # form_with 用
+          @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+        end
     end
 
     def show
@@ -12,13 +19,14 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new (task_params)
+        @task = current_user.tasks.build (task_params)
         if @task.save
             flash[:success] = "タスクリストに追加されました"
-            redirect_to @task
+            redirect_to root_url
         else
+            @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
             flash.now[:danger] = 'タスクリストに追加できませんでした'
-            render :new
+            render tasks/index
         end
     end
 
@@ -39,11 +47,10 @@ class TasksController < ApplicationController
     end
 
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクは正常に削除されました'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
      private
@@ -51,6 +58,13 @@ class TasksController < ApplicationController
      # Strong Parameter
      def task_params
         params.require(:task).permit(:content, :status)
+     end
+     
+     def correct_user
+       @task = current_user.tasks.find_by(id: params[:id])
+       unless @task
+         redirect_to root_url
+       end
      end
      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
 
   get 'login', to: 'sessions#new'
@@ -10,5 +7,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :tasks, only: [:create, :destroy]
   resources :tasks
 end
