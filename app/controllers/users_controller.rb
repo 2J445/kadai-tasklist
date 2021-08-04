cb2359c1207ad1552d21795ec9d5a164bd349b2a
shogo.tsuji <tjsg06@star.cims.jp>
2021-08-04 05:59:04 +0000

    tasklist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,12 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
   
-  def index
-  end
-
-  def show
-  end
-
-  def new
-    @user = User.new
-  end
-
+  
   def create
     @user = User.new(user_params)
     
     if @user.save
       flash[:success] = 'ユーザを登録しました'
-      redirect_to @user
+      redirect_to login_url
     else
       flash[:danger] = 'ユーザを登録できませんでした'
       render :new
